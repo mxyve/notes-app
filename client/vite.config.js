afcf4f50b280e7d99d9a464a44274769eb98d0ea
缪心雨 <1286280961@qq.com>
2025-04-24 15:29:10 +0800

    第一版

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import UnoCSS from 'unoCss/vite';
+import UnoCSS from 'unocss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
